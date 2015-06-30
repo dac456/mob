e9778b8d27e9297b80ec8323dfea9c47ec4e96b5
Daniel Cook <daniel@danielcook.me>
2015-06-30 16:51:49 -0230

    Updated for Win32 compatibility.

--- mobnode/include/Common.h
+++ mobnode/include/Common.h
@@ -1,6 +1,10 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
+#if defined(__WIN32__) || defined(_WIN32) || defined(WIN32)
+    #define _WIN32_WINNT 0x0501
+#endif
+
 #include <memory>
 #include <vector>
 #include <utility>
